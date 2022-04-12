a0a417753138295d618b1f2335e04dac33aa10d0
falook555 <falook555@gmail.com>
2022-04-12 14:55:57 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,30 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useEffect } from 'react'
+
+const liffId = process.env.NEXT_PUBLIC_LIFF_ID
 
 function MyApp({ Component, pageProps }) {
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const liff = (await import('@line/liff')).default
+
+      try {
+        await liff.init({ liffId })
+      }
+      catch (error) {
+        console.error('liff error', error.message)
+      }
+
+      if (!liff.isLoggedIn()) {
+        liff.login()
+      }
+    }
+    fetchData()
+  }, [])
+  
   return <Component {...pageProps} />
 }
 
