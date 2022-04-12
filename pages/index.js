c0c9bce33dab8e4798e27b8286e09a9085572bed
falook555 <falook555@gmail.com>
2022-04-12 15:58:02 +0700

    n

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+
 
 export default function Home() {
   const router = useRouter()
