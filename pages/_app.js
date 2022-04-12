0e75ccf5e224e1260b8b190295beca2fd757800b
falook555 <falook555@gmail.com>
2022-04-12 16:05:14 +0700

    ss

--- pages/_app.js
+++ pages/_app.js
@@ -19,9 +19,9 @@ function MyApp({ Component, pageProps }) {
       }
 
       if (!liff.isLoggedIn()) {
-       
-
-        liff.login()
+        let path  = localStorage.getItem('path')
+        
+        liff.login({ redirectUri : `https://queue.diligentsoftinter.com/${path}` })
         
       }
     }
